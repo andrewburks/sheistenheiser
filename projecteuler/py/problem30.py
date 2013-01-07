877b76e2ef700a798840e4a74d3aa103e55876d3
andrewburks <andrew.burks@gmail.com>
2013-01-06 23:15:27 -0600

    fixed/cleaned up problem 30

--- projecteuler/py/problem30.py
+++ projecteuler/py/problem30.py
@@ -2,31 +2,14 @@
 from common import timed_func
 from math import floor, log10
 
-
-def upperlimit(n, l):
-    return 9 ** n * l
-
-def lennum(n):
-    return int(floor(log10(n))) + 1
-
-
 @timed_func
 def problem30(n):
     narcissists = []
     
-    # determine search ranges...
-    p9 = 9 ** n
-    ranges = []
     start = 2
-    for x in xrange(2, n + 1):
-        
+    end = 10 ** (n + 1)
 
-    f = 2
-    t = 10 ** (n + 1)
-    
-    print 'from: %s to: %s' % (f, t)
-
-    for x in xrange(f, t):
+    for x in xrange(start, end):
         s = 0
         l = int(floor(log10(x))) + 1
         for y in xrange(l, 0, -1):
